a91d5fa26a8e049b7d0551aa0804c00ed61fba22
metrobg <egg@metrobg>
2020-03-26 16:12:49 -0400

    Added code to close the session and the connection.

--- SendMessage/SendMessage.cs
+++ SendMessage/SendMessage.cs
@@ -16,15 +16,21 @@ namespace SendMessage
             String local_host  = null;
             String remote_host = null;
             String in_out      = null;
+            bool TEST = true;
 
             username     = Arg(args, 0, "unknown_user");
             local_host   = Arg(args, 1, "unknown_local");
             remote_host  = Arg(args, 2, "unknown_remote");
             in_out       = Arg(args, 3, "IN");
-
+            if (TEST)
+            {
+                
+                password = "admin";
+                host = "192.168.10.66";
+            }
             DateTime now = DateTime.Now;
 
-            string text = "User=" + username + ";RemoteHost=" + remote_host + ";ComputerName=" + local_host + ";TimeStamp=" + now;
+            string text = "User=" + username + ";RemoteHost=" + remote_host + ";ComputerName=" + local_host + ";TimeStamp=" + now + ";in_out=" + in_out.ToUpper();
             SendNewMessage(text, host, password, user);
         }
 
@@ -33,7 +39,7 @@ namespace SendMessage
             //string topic = "TextQueue";
             string topic = "GPDC-LOGIN";
 
-            Console.WriteLine($"Adding message to queue topic: {topic}");
+            // Console.WriteLine($"Adding message to queue topic: {topic}");
 
             String brokerUri = "activemq:tcp://" + host + ":" + "61616";
             NMSConnectionFactory factory = new NMSConnectionFactory(brokerUri);
@@ -47,11 +53,18 @@ namespace SendMessage
                 using (IMessageProducer producer = session.CreateProducer(dest))
                 {
                     producer.DeliveryMode = MsgDeliveryMode.NonPersistent;
-
                     producer.Send(session.CreateTextMessage(text));
-                    Console.WriteLine($"Sent {text} messages");
+                    Console.WriteLine($"Sending: {text} ");
+
+                    session.Dispose();
                 }
+                if (connection.IsStarted)
+                {
+                    connection.Close();
+                }
+
             }
+
         }
         private static String Env(String key, String defaultValue)
         {
